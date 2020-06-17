2bea1f2be8fc24df71157c1009733ec15e8f26d4
Sergio Jaume <jaume.sergio101@gmail.com>
2020-06-16 20:32:35 -0600

    Updated with plotting of poll results

--- analyze.R
+++ analyze.R
@@ -1,9 +1,15 @@
+#declare libraries
+library(tidyverse)
+library(ggplot2)
+library(tidyr)
+
+#_______________
 # #download raw fivethirtyeight data
 # url <- "https://projects.fivethirtyeight.com/polls-page/president_polls.csv"
 # destination_file <- "data/polls.csv"
 # download.file(url, destination_file)
 # 
-# wrangle raw data
+# #wrangle raw data
 # polls <- read_csv("data/polls.csv")
 # polls <- polls %>% subset(select = -c(race_id, seat_number, seat_number,
 #                                       url, sponsor_ids, pollster_id,
@@ -11,10 +17,7 @@
 # 
 # save(polls, file = "rdas/polls.rda")
 # load("rdas/polls.rda")
-
-#declare libraries
-library(tidyverse)
-library(ggplot2)
+#________________
 
 "Biden" %in% polls$answer
 select_col <- c("pollster", "state",
@@ -37,18 +40,50 @@ poll_spread <- filtered_poll %>%
          spread = party*abs(2*pct - 100))
 poll_spread
 
-#calculate avg and se
+#calculate avg and se, remove NAs
 #NA sd means that there was only one result for a pollster
-poll_calc <- poll_spread %>%
+poll_results <- poll_spread %>%
   group_by(pollster) %>%
-  summarize(avg = mean(spread), se = sd(spread)/sqrt(length(spread))) %>%
-  mutate(start = avg - 1.96*se, end = avg + 1.96*se)
-poll_calc
+  summarize(avg = mean(spread), se=sd(spread)/sqrt(length(spread))) %>%
+  mutate(start = avg - 1.96*se, end = avg + 1.96*se) %>%
+  drop_na()
+poll_results
 
-#TEST ZONE
-####################
+#Computing the posterior mean, standard error, credible interval and probability
+#mu is the predicted spread, tau is the average spread in past years
+mu <- 0
+tau <- 0.035
+sigma <- poll_results$se
+Y <- poll_results$avg
+B <- sigma^2 / (sigma^2 + tau^2)
+posterior_mean <- B*mu + (1-B)*Y
+posterior_se <- sqrt(1 / (1/sigma^2 + 1/tau^2))
 
-##################
+posterior_mean
+posterior_se
+# 95% credible interval of spread
+interval <- posterior_mean + c(-1.96, 1.96)*posterior_se
+# probability of d > 0 (ie. DEM winning)
+probability <- 1 - pnorm(0, posterior_mean, posterior_se)
+mean(probability)
+
+#add the probability of DEMS winning, subtract 50 to show difference
+prediction <- poll_results %>% 
+  mutate(prob = probability-0.5) %>% 
+  arrange(desc(prob))
+prediction
+  
+#plot the probability for each pollster
+plot <- prediction %>% ggplot(aes(x=reorder(pollster,prob), y=prob, fill = prob>0)) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  geom_bar(stat = "identity") +
+  labs(title = "Probability of DEM Winning by Pollster", 
+       subtitle = "Last updated 06-16-2020",
+       x = "Pollster Name", 
+       y = "Probability of Win",
+       caption = "Source: fivethirtyeight (https://projects.fivethirtyeight.com/polls-page/president_polls.csv)",
+       fill = "Winning Party") +
+  scale_fill_discrete(name = "Winning Party", labels = c("Republican", "Democrat"))
+plot
 
 # ggsave("figs/barplot.png")
-
