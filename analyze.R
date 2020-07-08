53f0da48167a997dc7f0d76ff3a9ff81c0c6cf29
Sergio Jaume <jaume.sergio101@gmail.com>
2020-07-08 10:34:20 -0600

    Removed original spread calculations andplotted based on party (not pollster)

--- analyze.R
+++ analyze.R
@@ -24,66 +24,68 @@ select_col <- c("pollster", "state",
                 "sample_size", 
                 "end_date", "fte_grade",
                 "answer", "pct")
-select_grade <- c("A+", "A", "A-", "A/B", "B+")
+select_grade <- c("A+", "A", "A-", "A/B", "B+", "B")
 
 #select the desired columns, filter undesirable data
 filtered_poll <- polls %>% subset(select = select_col) %>%
   filter(end_date >= "4/1/20",
          fte_grade == select_grade | is.na(fte_grade),
          answer == c("Biden","Trump")) 
-filtered_poll
 
-#add spread and party
+#add party
 #-1 represents REP, 1 represents DEM
 poll_spread <- filtered_poll %>% 
-  mutate(party = ifelse(answer == "Trump", -1, 1),
-         spread = party*abs(2*pct - 100))
+  #removed spread column due to data quality
+  mutate(party = ifelse(answer == "Trump", -1, 1))
 poll_spread
 
 #calculate avg and se, remove NAs
 #NA sd means that there was only one result for a pollster
 poll_results <- poll_spread %>%
-  group_by(pollster) %>%
-  summarize(avg = mean(spread), se=sd(spread)/sqrt(length(spread))) %>%
-  mutate(start = avg - 1.96*se, end = avg + 1.96*se) %>%
-  drop_na()
+  group_by(party) %>%
+  summarize(avg = mean(pct), se=sd(pct)/sqrt(length(pct))) %>%
+  mutate(start = avg - 1.96*se, end = avg + 1.96*se)
 poll_results
 
-#Computing the posterior mean, standard error, credible interval and probability
-#mu is the predicted spread, tau is the average spread in past years
-mu <- 0
-tau <- 0.035
-sigma <- poll_results$se
-Y <- poll_results$avg
-B <- sigma^2 / (sigma^2 + tau^2)
-posterior_mean <- B*mu + (1-B)*Y
-posterior_se <- sqrt(1 / (1/sigma^2 + 1/tau^2))
+#subtract each number to calc avg spread
+#if negative, rep; if positive, dem
+spread <- poll_results[2, ] - poll_results[1, ]
+spread
 
-posterior_mean
-posterior_se
+spread_mean <- spread$avg
+spread_mean
+spread_se <- spread$se
+spread_se
+
 # 95% credible interval of spread
-interval <- posterior_mean + c(-1.96, 1.96)*posterior_se
+interval <- spread_mean + c(-1.96, 1.96)*spread_se
+interval
 # probability of d > 0 (ie. DEM winning)
-probability <- 1 - pnorm(0, posterior_mean, posterior_se)
+probability <- 1 - pnorm(0, spread_mean, spread_se)
 mean(probability)
 
 #add the probability of DEMS winning, subtract 50 to show difference
 prediction <- poll_results %>% 
-  mutate(prob = probability-0.5) %>% 
+  mutate(prob = ifelse(sign(probability)==sign(party), 
+                       probability, 
+                       1-probability),
+         par_name = ifelse(party == -1, "Rep", "Dem")) %>% 
   arrange(desc(prob))
 prediction
   
-#plot the probability for each pollster
-plot <- prediction %>% ggplot(aes(x=reorder(pollster,prob), y=prob, fill = prob>0)) +
+#plot the probability for each party
+plot <- prediction %>% ggplot(aes(x=reorder(par_name,prob), y=prob, fill = prob>0.5)) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
   geom_bar(stat = "identity") +
-  labs(title = "Probability of DEM Winning by Pollster", 
-       subtitle = "Last updated 06-16-2020",
-       x = "Pollster Name", 
+  labs(title = "Probability of a Party Winning", 
+       subtitle = "Last updated 07-08-2020",
+       x = "Party Name", 
        y = "Probability of Win",
        caption = "Source: fivethirtyeight (https://projects.fivethirtyeight.com/polls-page/president_polls.csv)",
        fill = "Winning Party") +
   scale_fill_discrete(name = "Winning Party", labels = c("Republican", "Democrat"))
 plot
+#In the future, I would like to create a graph that looks like
+#a "tug of war over the 50% line
 
-# ggsave("figs/barplot.png")
+ggsave("figs/barplot.png")
